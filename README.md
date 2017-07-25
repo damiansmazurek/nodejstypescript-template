526afe6f9fbde49522a7084bab3da5089208bd1a
damian.mazurek <damian.mazurek@elmodis.com>
2017-07-25 23:48:41 +0200

    Template initialization
    * Typescript configuration
    * Npm commands
    * Tests in mocha chai

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # nodejstypescript-template
+
+1. Install mocha chai global: npm install mocha chai ts-node -g
+2. Run npm install in console.
+3. Start developing
---
+++ tests/template.test.ts
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+ import 'mocha';
+
+describe('Class describe', () => {
+  it('Test name', () => {
+    const result = 1;
+
+    expect(result).to.equal(1);
+  });
+});
